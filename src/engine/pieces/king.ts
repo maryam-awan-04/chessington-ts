88221121c75fce724e45f653b61ce47656cfd797
Maryam Awan <maryamawan04@gmail.com>
2022-10-14 14:39:45 +0100

    King can move to adjacent squares and cannot make any other moves

--- src/engine/pieces/king.ts
+++ src/engine/pieces/king.ts
@@ -7,7 +7,49 @@ export class King extends Piece {
     super(player)
   }
 
-  getAvailableMoves(_board: Board): Square[] {
-    return [] as Square[]
+  getAvailableMoves(board: Board): Square[] {
+    const currentSquare = board.findPiece(this)
+
+    const north = Square.at (
+      currentSquare.row + 1,
+      currentSquare.col,
+    )
+
+    const northEast = Square.at (
+      currentSquare.row + 1,
+      currentSquare.col + 1,
+    )
+
+    const east = Square.at (
+      currentSquare.row,
+      currentSquare.col + 1,
+    )
+
+    const southEast = Square.at (
+      currentSquare.row - 1,
+      currentSquare.col + 1,
+    )
+
+    const south = Square.at (
+      currentSquare.row - 1,
+      currentSquare.col,
+    )
+
+    const southWest = Square.at (
+      currentSquare.row - 1,
+      currentSquare.col - 1,
+    )
+
+    const west = Square.at (
+      currentSquare.row,
+      currentSquare.col - 1,
+    )
+
+    const northWest = Square.at (
+      currentSquare.row + 1,
+      currentSquare.col - 1,
+    )
+
+    return [north, northEast, east, southEast, south, southWest, west, northWest]
   }
 }
